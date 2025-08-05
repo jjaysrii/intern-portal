4a462d3d41048d3c546629d69e2e5210fb00f34a
Chellarpu Jayasri <135418754+jjaysrii@users.noreply.github.com>
2025-08-05 21:15:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors()); // Allow frontend to access backend
+
+// Example data
+const interns = [
+  {
+    name: "Jayasri",
+    referralCode: "jayasri2055",
+    amountRaised: 12500
+  },
+  { name: "Priya", referralCode: "priya2025", amountRaised: 10000 },
+  { name: "Anjali", referralCode: "anjali2025", amountRaised: 8000 },
+  { name: "Rahul", referralCode: "rahul2025", amountRaised: 6000 }
+];
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Intern Portal API!');
+});
+
+// API endpoint
+app.get('/api/interns', (req, res) => {
+  res.json(interns);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
